175c23bcb26b0b3a598db544ca6945c8b4fd8145
gustavo veloso <gugaveloso12@gmail.com>
2026-06-03 14:08:26 -0300

    feat: migration de criação da tabela users

--- db/migrations/20260603150434_create_users_table.ts
+++ db/migrations/20260603150434_create_users_table.ts
@@ -1,10 +1,17 @@
 import type { Knex } from "knex";
 
 
+
 export async function up(knex: Knex): Promise<void> {
+    await knex.schema.createTable('users', (table) => {
+        table.uuid('id').primary()
+        table.uuid('session_id').nullable()
+        table.text('name').notNullable()
+    })
 }
 
 
 export async function down(knex: Knex): Promise<void> {
+    await knex.schema.dropTable('users')
 }
 
