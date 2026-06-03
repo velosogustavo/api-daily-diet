42891e46b2d7ca6b0b2c6f08d18b7556377e4138
gustavo veloso <gugaveloso12@gmail.com>
2026-06-03 14:28:38 -0300

    feat: migration de criação da tabela meals

---
+++ db/migrations/20260603170927_create_meals_table.ts
@@ -0,0 +1,19 @@
+import type { Knex } from "knex";
+
+
+export async function up(knex: Knex): Promise<void> {
+    await knex.schema.createTable('meals', (table) => {
+        table.uuid('id').primary()
+        table.uuid('user_id').references('id').inTable('users').notNullable()
+        table.text('name').notNullable()
+        table.text('description').notNullable()
+        table.boolean('is_on_diet').notNullable()
+        table.timestamp('created_at').defaultTo(knex.fn.now()).notNullable()
+    })
+}
+
+
+export async function down(knex: Knex): Promise<void> {
+    await knex.schema.dropTable('meals')
+}
+
